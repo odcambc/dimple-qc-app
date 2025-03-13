aa5df4d227a722aa23fc1379247cb1e3f5a5ac65
Chris <17653365+odcambc@users.noreply.github.com>
2025-03-13 07:35:32 -0500

    rm plotly

--- plots.py
+++ plots.py
@@ -1,52 +1,7 @@
-import plotly.express as px
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from matplotlib.lines import Line2D
-
-
-def position_vs_value_plot_plotly(per_base_df, displayed_fields, range):
-    vals = per_base_df[stat]
-    vals = vals[~vals.isnull()]
-    fig = ff.create_distplot(
-        [vals],
-        ["Overall"],
-        rug_text=[careers_df["player_name"]],
-        colors=["black"],
-        show_hist=False,
-    )
-    # Clean up some defaults (1st trace is the density plot, 2nd is the rug plot)
-    fig.data[0].hoverinfo = "none"
-    fig.data[0].showlegend = False
-    fig.data[1].hoverinfo = "text+x"
-    fig.data[1].customdata = careers_df["person_id"]
-    # Use height of the density plot to inform the vertical lines
-    ymax = fig.data[0].y.max()
-    # Arrange rows from highest to lowest value so that legend order is correct
-    stats_df = stats_df.sort_values(stat, ascending=False)
-    # Add vertical lines for each player
-    for _, row in stats_df.iterrows():
-        x = row[stat]
-        fig.add_scatter(
-            x=[x, x],
-            y=[0, ymax],
-            mode="lines",
-            name=players_dict[row["person_id"]],
-            line=dict(color=row["color"], width=1),
-            hoverinfo="x+name",
-        )
-
-    fig.update_layout(
-        hovermode="x",
-        xaxis=dict(title=stat + " per game (career average)", hoverformat=".1f"),
-        legend=dict(orientation="h", y=1.03, yanchor="bottom", x=0.5, xanchor="center"),
-    )
-
-    # Convert Figure to FigureWidget so we can add click events
-    fig = go.FigureWidget(fig.data, fig.layout)
-    fig.data[1].on_click(on_rug_click)
-
-    return fig
 
 
 def position_vs_value_plot(
